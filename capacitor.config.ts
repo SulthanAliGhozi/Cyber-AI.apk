1a3a3d0548aa499051d2c223a9b7959d06bc9ec7
Sulthan Ali Ghozi <160091167+SulthanAliGhozi@users.noreply.github.com>
2026-07-19 10:26:58 +0700

    Update Capacitor hostname for Firebase Auth

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cyber.ai.app',
   appName: 'Cyber AI',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    hostname: 'cyber-ai-gacor.vercel.app',
+    androidScheme: 'https'
+  }
 };
 
 export default config;
